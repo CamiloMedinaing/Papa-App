5c12dd4eaa13f9ab86872842f0708e10147bedc7
Paulo Coronado <cjmedinag@udsitrital.edu.co>
2026-05-27 15:58:02 -0500

    v.5.0

--- backend/name.py
+++ backend/name.py
@@ -1,8 +1,42 @@
-#paquetes:fastapi,uvicorn
-from fastapi 
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+app = FastAPI()
 
+# Lista donde se guardan los agricultores
+agricultores = []
 
-@app.get("crear_agricultor")
-def crear_agricultor():
-    return "cambiar esto por la funcionalidad"
+# Modelo del agricultor
+class Agricultor(BaseModel):
+    id: int
+    cedula: str
+    nombre: str
+    area: float
+    cultivo: str
+    inversion: float
+    fecha: str
+    ubicacion_cultivo: str
+
+# Ruta principal
+@app.get("/")
+def inicio():
+    return {
+        "mensaje": "Servidor funcionando",
+        "cantidad_agricultores": len(agricultores)
+    }
+
+# Registrar agricultor
+@app.post("/crear_agricultor")
+def crear_agricultor(agricultor: Agricultor):
+
+    agricultores.append(agricultor.dict())
+
+    return {
+        "mensaje": "Agricultor registrado correctamente",
+        "datos": agricultor
+    }
+
+# Mostrar agricultores
+@app.get("/agricultores")
+def mostrar_agricultores():
+    return agricultores
